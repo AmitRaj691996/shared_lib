7e7b1c8455ebf15b6c82f01a7b9b4232de3abced
AmitRaj691996 <131245424+AmitRaj691996@users.noreply.github.com>
2024-05-02 16:37:36 +0530

    Update auditTools2.groovy

--- vars/auditTools2.groovy
+++ vars/auditTools2.groovy
@@ -1,10 +1,10 @@
 def call(Map config){
     node{
-      sh '''
-      echo "${config.message}"
-      git --version
-      sudo apt-get install maven -y
-      mvn --version
-      '''
+          sh '''
+          echo "${config.message}"
+          git --version
+          sudo apt-get install maven -y
+          mvn --version
+          '''
     }
 }
