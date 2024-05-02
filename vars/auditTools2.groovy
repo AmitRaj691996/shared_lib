00803708edd28dbe9a6d66d90512382af7867ae5
AmitRaj691996 <131245424+AmitRaj691996@users.noreply.github.com>
2024-05-02 17:10:54 +0530

    Update auditTools2.groovy

--- vars/auditTools2.groovy
+++ vars/auditTools2.groovy
@@ -2,7 +2,7 @@ def call(Map config){
     node{
           echo "${config.message}"
           sh '''
-          echo " Your Build number is: ${env.BUILD_NUMBER}"
+          echo "Your Build number is: ${BUILD_NUMBER}"
           git --version
           sudo apt-get install maven -y
           mvn --version
